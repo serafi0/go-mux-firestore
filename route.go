acf78b3a26a3dd90c9d25715b454ad39eb8ec3d4
mohamedelserafy <elserafy02@gmail.com>
2021-02-19 21:52:23 +0200

    get posts

---
+++ route.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+)
+type Post struct {
+	Id int `json: "id"`
+	Title string `json: "title"`
+	Text string `json: "text"`
+
+}
+
+var (
+	posts[] Post
+)
+
+func init(){
+	posts= []Post {Post{Id:1,Title: "Title 1",Text:"Text 1"}}
+}
+
+func getPosts(resp http.ResponseWriter, req *http.Request){
+
+	resp.Header().Set("Content-type","application/json")
+	result, err :=json.Marshal(posts)
+	if(err!= nil){
+		 resp.WriteHeader(http.StatusInternalServerError)
+		 resp.Write([]byte(`{"error": Error marshalling the posts array }`))
+		 return
+	}
+
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(result)
+
+}
